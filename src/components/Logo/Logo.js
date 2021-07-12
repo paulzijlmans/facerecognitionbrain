31acb8a682c8d5e10a3411176b05e65ff8be9151
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-07-12 22:07:09 +0200

    Building components - added Navigation - added Logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className= 'Tilt br2 shadow-2'>
+        <div style={{ height: '150px', width: '150px' }}> 👀 </div>
+      </Tilt>
+    </div>
+  )
+}
+
+export default Logo;
