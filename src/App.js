8175c69f086d18f040c00a6f8bcf208b70d89ab2
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-07-19 22:10:24 +0200

    Building Our Components - updated background - added ImageLinkForm - added Rank

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
 import React, { Component } from 'react';
+import Particles from "react-tsparticles";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
+const particlesOptions = {
+  fpsLimit: 60,
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 30,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          options={particlesOptions}
+        />
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm />
-        <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition />  */}
       </div>
     );
   }
