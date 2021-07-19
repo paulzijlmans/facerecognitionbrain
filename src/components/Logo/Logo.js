3722d52bbc4120a3b02e4a1a12a2597a424111d1
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-07-19 21:25:22 +0200

    Building components - completed Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
 import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
-      <Tilt className= 'Tilt br2 shadow-2'>
-        <div style={{ height: '150px', width: '150px' }}> 👀 </div>
+    <div className='ma4 mt0' style={{ width: '150px' }}>
+      <Tilt className='Tilt br2 shadow-2'>
+        <div className='pa3' style={{ height: '150px' }}>
+          <img style={{ paddingTop: '5px' }} alt='logo' src={brain} height='100' />
+        </div>
       </Tilt>
     </div>
   )
